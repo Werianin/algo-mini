4e613567293ca9765dc3b109ae35f1471f799f6a
Werianin <makanin.fed@gmail.com>
2025-02-11 03:09:36 +0700

    Update solution

--- mini_03.py
+++ mini_03.py
@@ -1,5 +1,7 @@
 import subprocess
+import os
 import argparse
+import shutil
 
 def run_cmd(cmd, cwd):
     result = subprocess.run(cmd, shell=True, cwd=cwd, stdout=subprocess.DEVNULL,
@@ -39,13 +41,32 @@ def git_bisect(path, good, bad, test_cmd):
     subprocess.run(f'git checkout {main_commit}', shell=True, cwd=path,
                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
+def clone_repo(url, path):
+    result = run_cmd(f'git clone -q {url} {path}', '.')
+    if result != 0:
+        raise Exception(f"Failed to clone repository: {url}")
+    return path
+
+# input: 
+# python3 mini_03.py https://github.com/Werianin/test-repo f92a601b711aecec418d008fc174d9dde08d1a39 f32479a8796d549c3974b4d19b51a8658a9c0643 "python3 main.py"
+
+# output:
+# Bad commit: efeac37fa7bddca62ac31c43eb142003ad41958e
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("path")
+    parser.add_argument("url")
     parser.add_argument("good")
     parser.add_argument("bad")
     parser.add_argument("test_cmd")
     
     args = parser.parse_args()
-    git_bisect(args.path, args.good, args.bad, args.test_cmd)
+
+    temp_dir = "temp_dir"
+    os.mkdir(temp_dir)
+    path = clone_repo(args.url, temp_dir)
+    git_bisect(path, args.good, args.bad, args.test_cmd)
+
+    shutil.rmtree(path)
+
 
