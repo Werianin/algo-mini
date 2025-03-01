f150477ed6f7b6c82f37dc2ff1d249682e425a3b
Werianin <makanin.fed@gmail.com>
2025-03-01 19:36:26 +0700

    Update solution

--- mini_11.c
+++ mini_11.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
-
-typedef struct {
-    int a;
-    int b;
-} pair;
 
 void swap(int* a, int* b) {
   int temp = *a;
@@ -12,63 +8,59 @@ void swap(int* a, int* b) {
   *b = temp;
 }
 
-pair Lomuto(int* arr, size_t len) {
+void quickSort_Lomuto(int* arr, size_t len) {
+    if (len < 1) {
+        return;
+    }
+    srand(time(NULL));
+    size_t pivot = random() % len;
+    swap(arr, arr + pivot);
     size_t i = 1;
-    for (size_t j = 1; j < len; j++) {
+    for  (size_t j = 1; j < len; j++) {
         if (arr[j] < arr[0]) {
             swap(arr + i, arr + j);
             i++;
         }
     }
     swap(arr, arr + i - 1);
-    pair res;
-    res.a = i - 1;
-    res.b = i - 1;
-    return res;
+    quickSort_Lomuto(arr, i - 1);
+    quickSort_Lomuto(arr + i, len - i);
 }
 
-pair Hoare(int* arr, size_t len) {
-    size_t beg = 0;
-    size_t end = 0;
-    for (size_t i = 1; i < len; i++) {
-        if (arr[i] < arr[0]) {
-            int tmp = arr[i];
-            arr[i] = arr[end + 1];
-            arr[end + 1] = arr[beg];
-            arr[beg] = tmp;
-            beg++;
-            end++;
-        }
-        else if (arr[i] == arr[0]) {
-            swap(arr + end + 1, arr + i);
-            end++;
-        }
+int Hoare(int* arr, int beg, int end)
+{
+    srand(time(NULL));
+    size_t piv_i = beg + random() % (end - beg);
+    swap(arr + beg, arr + piv_i);
+
+    size_t pivot = arr[beg];
+    size_t i = beg - 1, j = end + 1;
+ 
+    while (1) {
+        do {i++;} while (arr[i] < pivot);
+        do {j--;} while (arr[j] > pivot);
+        if (i >= j) return j;
+        swap(arr + i, arr + j);
     }
-    pair res;
-    res.a = beg;
-    res.b = end;
-    return res;
 }
-
-void quickSort(int* arr, size_t len, pair partition(int*, size_t)) {
-    if (len < 1) {
+ 
+void quickSort_Hoare(int* arr, int beg, int end)
+{
+    if (beg >= end) {
         return;
     }
-    size_t pivot = random() % len;
-    swap(arr, arr + pivot);
-    pair pivs = partition(arr, len);
-    quickSort(arr, pivs.a, partition);
-    quickSort(arr + pivs.b + 1, len - pivs.b - 1, partition);
+    int piv_i = Hoare(arr, beg, end);
+    quickSort_Hoare(arr, beg, piv_i);
+    quickSort_Hoare(arr, piv_i + 1, end);
 }
 
 int main() {
-    size_t len = 8;
+    size_t len = 10;
     int* arr = (int*) malloc(len * sizeof(int));
     for (size_t i = 0; i < len; i++) {
         scanf("%d", arr + i);
     }
-    //quickSort(arr, len, Hoare);
-    Hoare(arr, len);
+    quickSort_Hoare(arr, 0, len - 1);
     for (size_t i = 0; i < len; i++) {
         printf("%d ", arr[i]);
     }
