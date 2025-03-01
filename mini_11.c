d73a56c73d01198a9cda8cee37676a4b9c0d7b49
Werianin <makanin.fed@gmail.com>
2025-03-01 20:05:54 +0700

    Update solution

--- mini_11.c
+++ mini_11.c
@@ -13,28 +13,37 @@ void quickSort_Lomuto(int* arr, size_t len) {
         return;
     }
     srand(time(NULL));
-    size_t pivot = random() % len;
-    swap(arr, arr + pivot);
-    size_t i = 1;
-    for  (size_t j = 1; j < len; j++) {
-        if (arr[j] < arr[0]) {
-            swap(arr + i, arr + j);
-            i++;
+    size_t piv_i = random() % len;
+    swap(arr, arr + piv_i);
+    int pivot = arr[0];
+    size_t l, h;
+    l = h = 0;
+    for (size_t i = 1; i < len; i++) {
+        if (arr[i] < pivot) {
+            int tmp = arr[i];
+            arr[i] = arr[h + 1];
+            arr[h + 1] = arr[l];
+            arr[l] = tmp;
+            l++;
+            h++;
+        }
+        else if (arr[i] == pivot) {
+            swap(arr + h + 1, arr + i);
+            h++;
         }
     }
-    swap(arr, arr + i - 1);
-    quickSort_Lomuto(arr, i - 1);
-    quickSort_Lomuto(arr + i, len - i);
+    quickSort_Lomuto(arr, l);
+    quickSort_Lomuto(arr + h + 1, len - h - 1);
 }
+
 
 int Hoare(int* arr, int beg, int end)
 {
-    srand(time(NULL));
-    size_t piv_i = beg + random() % (end - beg);
+    int piv_i = beg + random() % (end - beg);
     swap(arr + beg, arr + piv_i);
 
-    size_t pivot = arr[beg];
-    size_t i = beg - 1, j = end + 1;
+    int pivot = arr[beg];
+    int i = beg - 1, j = end + 1;
  
     while (1) {
         do {i++;} while (arr[i] < pivot);
@@ -43,7 +52,7 @@ int Hoare(int* arr, int beg, int end)
         swap(arr + i, arr + j);
     }
 }
- 
+
 void quickSort_Hoare(int* arr, int beg, int end)
 {
     if (beg >= end) {
@@ -54,13 +63,16 @@ void quickSort_Hoare(int* arr, int beg, int end)
     quickSort_Hoare(arr, piv_i + 1, end);
 }
 
+
+
+
 int main() {
-    size_t len = 10;
+    size_t len = 8;
     int* arr = (int*) malloc(len * sizeof(int));
     for (size_t i = 0; i < len; i++) {
         scanf("%d", arr + i);
     }
-    quickSort_Hoare(arr, 0, len - 1);
+    quickSort_Lomuto(arr, len);
     for (size_t i = 0; i < len; i++) {
         printf("%d ", arr[i]);
     }
