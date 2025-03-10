83a0abd0133253f6dcb3e50ca3cb76fface8876e
Werianin <makanin.fed@gmail.com>
2025-03-10 20:44:43 +0700

    Update solution

--- mini_17.py
+++ mini_17.py
@@ -5,25 +5,22 @@ class ListNode:
         self.val = val
         self.next = next
 
-    def __lt__(self, another):
-        return self.val < another.val
-
 def merge_sorted_lists(lists):
     heap = []
 
-    for node in lists:
-        if node:
-            heapq.heappush(heap, (node.val, node))
+    for l in lists:
+        if l:
+            heapq.heappush(heap, (l.val, l))
 
     start = ListNode(0)
     cur = start
 
     while heap:
-        val, node = heapq.heappop(heap)
+        val, l = heapq.heappop(heap)
         cur.next = node
         cur = cur.next
 
         if node.next:
-            heapq.heappush(heap, (node.next.val, node.next))
+            heapq.heappush(heap, (l.next.val, l.next))
 
     return start.next
