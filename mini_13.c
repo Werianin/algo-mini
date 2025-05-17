0349bef0ddf8d2205ecd5c2c4da1ccf6deb53867
Werianin <makanin.fed@gmail.com>
2025-05-17 11:06:19 +0700

    Update solution

--- mini_13.c
+++ mini_13.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <assert.h>
+
+#define TEST_COUNT 10000
 
 void swap(int* a, int* b) {
   int temp = *a;
@@ -33,38 +36,61 @@ int* lomuto_partition_branchfree(int* first, int* last) {
     return first;
 }
 
-size_t main_oil_pipeline(int* arr, size_t len, int* first, int* last) {
-    size_t mid = len / 2;
-
-    if (first >= last && first - arr != mid) {
-        exit(1);
+void quickSort_LomutoBranchfree(int* first, int* last) {
+    if (first >= last) {
+        return;
     }
+    int* pivo = lomuto_partition_branchfree(first, last);
+    quickSort_LomutoBranchfree(first, pivo);
+    quickSort_LomutoBranchfree(pivo + 1, last);
+}
+
+double main_oil_pipeline(int* arr, size_t len,
+                         int* first, int* last,
+                         int* result, size_t* step) {
+    size_t mid1 = len / 2;
+    size_t mid2 = (len - 1) / 2;
 
     int* pivo = lomuto_partition_branchfree(first, last);
     size_t position = pivo - arr;
 
-    if (position == mid) {
-        return arr[position];
+    if (position == mid1 || position == mid2) {
+        if (mid1 == mid2) {
+            return arr[position];
+        }
+        else if (*step == 1) {
+            return ((double) (*result + arr[position])) / 2;
+        }
+        *result = arr[position];
+        *step = 1;
     }
-    if (mid < position) {
-        return main_oil_pipeline(arr, len, first, pivo);
+    if (mid1 < position || mid2 < position) {
+        return main_oil_pipeline(arr, len, first, pivo, result, step);
     }
-    if (position < mid) {
-        return main_oil_pipeline(arr, len, pivo + 1, last);
+    if (position < mid1 || position < mid2) {
+        return main_oil_pipeline(arr, len, pivo + 1, last, result, step);
     }
     return 0;
 }
 
 int main() {
     srand(time(NULL));
-    size_t len = 300;
-    int* arr = (int*) malloc(len * sizeof(int));
 
-    for (size_t i = 0; i < len; i++) {
-        arr[i] = rand() % 1000;
-        printf("%d ", arr[i]);
+    for (size_t t = 0; t < TEST_COUNT; t++) {
+        size_t len = rand() % 1000 + 1;
+        int* arr = (int*) malloc(len * sizeof(int));
+        for (size_t i = 0; i < len; i++) {
+            arr[i] = rand() % 10000;
+        }
+        int result = 0;
+        size_t step = 0;
+        double ans = main_oil_pipeline(arr, len, arr, arr + len, &result, &step);
+        quickSort_LomutoBranchfree(arr, arr + len);
+        double correct = ((double) (arr[len / 2] + arr[(len - 1) / 2])) / 2;
+
+        free(arr);
+        assert(ans == correct);
     }
-    size_t ans = main_oil_pipeline(arr, len, arr, arr + len);
-    printf("\n\n%zu is the best spot\n", ans);
+    printf("All %d tests passed!\n", TEST_COUNT);
 }
 
